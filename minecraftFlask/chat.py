95be15a3c9a6469662464fab43cb4aaf5d8eca39
ConfusedSky <masamaedae@gmail.com>
2018-09-25 19:13:05 -0700

    Makes the header bar automatically update when a player logs in or out.

--- minecraftFlask/chat.py
+++ minecraftFlask/chat.py
@@ -7,9 +7,36 @@ import cgi
 import socket
 import os
 from mcstatus import MinecraftServer
+from threading import Lock
 from . import socketio
 
 bp = Blueprint('index', __name__)
+thread = None
+thread_lock = Lock()
+
+def background_thread():
+    f = subprocess.Popen(['tail', '-F', '-n', '0', '/home/masa/Documents/MinecraftServers/Vanilla1-12-2/logs/latest.log'], stdout=subprocess.PIPE)
+    while True:
+        line = f.stdout.readline()
+        if not line:
+            break
+        # Do things with the line
+        if "joined" in line:
+            print("Emitting header update")
+            socketio.sleep(.5)
+            socketio.emit("update header")
+        if "left" in line:
+            print("Emitting header update")
+            socketio.sleep(2)
+            socketio.emit("update header")
+        print(line)
+
+@socketio.on('connect')
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None or not thread.isAlive():
+            thread = socketio.start_background_task(target=background_thread)
 
 @bp.route("/status")
 def status():
@@ -32,25 +59,15 @@ def status():
 @bp.route('/')
 def index():
     f = subprocess.Popen(['tail', '-n', '100', '/home/masa/Documents/MinecraftServers/Vanilla1-12-2/logs/latest.log'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    server = MinecraftServer("localhost", 25565)
 
-    query = None
     messages = []
+    while True:
+        line = f.stdout.readline()
+        if not line:
+            break
+        if "Server thread" not in line:
+            continue
+        line = line.split(" ")[0] + " " + ":".join(line.split(":")[3:])[1:]
+        messages.append(cgi.escape(line))
 
-    try:
-        query = server.query()
-
-        serverUp = True
-
-        while True:
-            line = f.stdout.readline()
-            if not line:
-                break
-            if "Server thread" not in line:
-                continue
-            line = line.split(" ")[0] + " " + ":".join(line.split(":")[3:])[1:]
-            messages.append(cgi.escape(line))
-    except socket.error:
-        serverUp = False
-
-    return render_template("template.jinja", serverUp=serverUp, query=query, messages=messages)
+    return render_template("template.jinja", messages=messages)
